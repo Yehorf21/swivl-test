07a6358746d7c438feae3b42bfecaf6ea14b1d37
Yehor-f2006 <151622806+Yehor-f2006@users.noreply.github.com>
2024-10-14 20:11:04 +0200

    Updates

--- assets/Feed-DYQljKd_.js
+++
@@ -1,1 +0,0 @@
-import{j as e,a as r,p as o}from"./index-BSIOzOz6.js";import{A as a}from"./Account-BiGa7SD9.js";import{C as t}from"./Card-1KuqiSHv.js";import"./index-Dr0JHPrF.js";const c=()=>e.jsxs("section",{className:"feed",children:[e.jsx(a,{profileInfo:r}),e.jsx("div",{className:"feed__cards",children:o.map(s=>e.jsx(t,{info:s,noShare:!0},s.title))})]});export{c as default};
