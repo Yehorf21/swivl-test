45ea4b5660509e0621ac4d6b8070d6fef879d892
Yehor-f2006 <151622806+Yehor-f2006@users.noreply.github.com>
2024-10-14 17:30:41 +0200

    Updates

---
+++ assets/Feed-CB6265Bs.js
@@ -0,0 +1,1 @@
+import{j as e,a as r,p as o}from"./index-CRes5yFW.js";import{A as a}from"./Account-CdEvG7tj.js";import{C as t}from"./Card-D34kzZrP.js";import"./index-Dy-jtEDr.js";const c=()=>e.jsxs("section",{className:"feed",children:[e.jsx(a,{profileInfo:r}),e.jsx("div",{className:"feed__cards",children:o.map(s=>e.jsx(t,{info:s,noShare:!0},s.title))})]});export{c as default};
--- assets/Feed-CLZhX-6J.js
+++
@@ -1,1 +0,0 @@
-import{j as e,a as r,p as o}from"./index-B8OfjyOG.js";import{A as a}from"./Account-Dz37LPAb.js";import{C as t}from"./Card-Be_NjTgs.js";import"./index-DEOMzT6m.js";const c=()=>e.jsxs("section",{className:"feed",children:[e.jsx(a,{profileInfo:r}),e.jsx("div",{className:"feed__cards",children:o.map(s=>e.jsx(t,{info:s,noShare:!0},s.title))})]});export{c as default};
